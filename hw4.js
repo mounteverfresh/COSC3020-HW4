28b8592e71ee97aef6dc745cf10dbb227e519036
Kyle Leadbetter <43153764+mounteverfresh@users.noreply.github.com>
2019-03-08 17:50:58 -0700

    Update hw4.js

--- hw4.js
+++ hw4.js
@@ -6,11 +6,14 @@ function permutationSort(arr)
 {
     var counter = 0;
     console.log("Sorting:" + arr)
+    var triedArr = [];
     while(!isSorted(arr))
     {
         counter++;
-        arr = newPermutation(arr);
+        arr = suffle(arr);
+        triedArr.push(arr.slice() );
     }
+    var contains = triedArr.includes(arr);
     console.log("Array sorted in " + counter
     + " permutations.\n" + arr)
 }
@@ -29,19 +32,27 @@ function isSorted(arr)
     return true;
 }
 
-function newPermutation(arr)
+function suffle(arr, tries)
 {
-    var newArr = arr;
-    var j;
-    for (var i = newArr.length; i > 0; i--)
-    {
-        j = Math.floor((Math.random()*i));
-        arr[i-1] = newArr[j];       
-        newArr.splice(j,1);
-    }
+    //var going = true;
+    //while(going)
+    //{
+        var newArr = arr.slice();
+        var j;
+        for (var i = newArr.length; i > 0; i--)
+        {
+            j = Math.floor((Math.random()*i));
+            arr[i-1] = newArr[j];       
+            newArr.splice(j,1);
+        }
+        //if(!tried.includes(arr.split()) )
+        //{going = false;}
+    //}
     return arr;
 }
-var testArr = [1,2,3,5,4,7,6,8,10,9]
-//permutationSort([1,2,3,5,4,7,6,8,10,9])
-console.log(testArr)
-console.log(newPermutation(testArr))
+//var testArr = [1,2,3,5,4,7,6,8,10,9]
+var testArr = [5,2,4,3,1];
+
+//console.log(  testArr + testArr.includes(t1.slice() )   )  
+permutationSort(testArr)
+   
